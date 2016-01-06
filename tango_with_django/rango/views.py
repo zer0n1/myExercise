e030a1288101149190b1e019d16c6ff19bc0577b
zer0n1 <zer0n1@163.com>
2016-01-06 14:28:32 +0800

    Doing exercise for add a new view about

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says: hey, hello world")
+    return HttpResponse("Rango says: hey, hello world</br><a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says: here is the about page.</br><a href='/rango/'>Index</a>")
