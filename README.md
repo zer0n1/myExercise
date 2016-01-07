81537fcb79b3737ddf8481e76c13fc1a61d82d69
zer0n1 <zer0n1@163.com>
2016-01-07 14:09:32 +0800

    Add new project for Offical Django Tutorial(v1.9)

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # ----20160105---- Tango
 Link:http://www.tangowithdjango.com/book17/index.html
+
+# ----20160107---- Official Django Turorials:1.9
+Link:https://docs.djangoproject.com/en/1.9/intro/tutorial01/
--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+"""
+# disabled old index view
 def index(request):
     return HttpResponse("Rango says: hey, hello world</br><a href='/rango/about'>About</a>")
+"""
+def index(request):
+    # Contruct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage':"I am bold font from the context"}
+
+    # Retrun a rendered response to send to the client
+    # we make use of the shortcut function to make our lives easier
+    # Note that the first parameter is the template we wish to use.
+    return render(request,'rango/index.html',context_dict)
 
 def about(request):
     return HttpResponse("Rango says: here is the about page.</br><a href='/rango/'>Index</a>")
